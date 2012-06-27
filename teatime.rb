a644313ecd4ee82373ce24c5f670f625c50a7352
Joshua Holt <holt.josh@gmail.com>
2012-06-27 15:38:47 -0400

    Fixing Tabs

--- teatime.rb
+++ teatime.rb
@@ -5,22 +5,22 @@ framework "QuartzCore"
 
 class Teatime
 
-	def run(time, flashes)
-		sleep time * 60
+  def run(time, flashes)
+    sleep time * 60
 
 
     fadeToken = Pointer.new('I')
     err = CGAcquireDisplayFadeReservation(15.0, fadeToken)
     if err == 0
-    	flashes.times do 
-    		CGDisplayFade(fadeToken[0], 0.2, 0.0, 1.0, 0.0, 0.0, 0.0, true)
-    		CGDisplayFade(fadeToken[0], 0.2, 1.0, 0.0, 0.0, 0.0, 0.0, true)
-    	end
+      flashes.times do 
+        CGDisplayFade(fadeToken[0], 0.2, 0.0, 1.0, 0.0, 0.0, 0.0, true)
+        CGDisplayFade(fadeToken[0], 0.2, 1.0, 0.0, 0.0, 0.0, 0.0, true)
+      end
     else
-    	puts "Couldn't acquire Fade Res..."
-		end
-		CGReleaseDisplayFadeReservation(fadeToken[0])
-	end
+      puts "Couldn't acquire Fade Res..."
+    end
+    CGReleaseDisplayFadeReservation(fadeToken[0])
+  end
 
 end
 
@@ -28,7 +28,7 @@ end
 duration = ARGV[0].to_i
 flashes  = 4
 if !ARGV[1].nil?
-	flashes = ARGV[1].to_i
+  flashes = ARGV[1].to_i
 end
 
 Teatime.new().run(duration, flashes)
